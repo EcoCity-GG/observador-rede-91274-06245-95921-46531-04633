6795094379de84cd75c1007fdd89bca80af7eddd
EcoCity <ecocity.gg@gmail.com>
2025-10-28 00:47:35 -0300

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -683,7 +683,7 @@ const Landing = () => {
               >
                 <Zap className="w-5 h-5 mr-2 group-hover:rotate-12 transition-transform" />
                 Começar Monitoramento
-              </Click>
+              </Button>
               <Button 
                 variant="outline"
                 className="border-2 border-white text-white hover:bg-white/10 rounded-xl px-8 py-6 text-lg hover:scale-105 transition-all duration-300"
